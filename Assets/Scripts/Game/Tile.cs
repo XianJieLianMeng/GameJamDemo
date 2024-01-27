42126950d563ca73d99ffeec7dfd256f977504d9
Liruirui <18335929360@163.com>
2024-01-27 11:28:26 +0800

    [feat]棋盘初始化

--- Assets/Scripts/Game/Tile.cs
+++ Assets/Scripts/Game/Tile.cs
@@ -2,22 +2,46 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
-
-public class Tile : MonoBehaviour, IPointerDownHandler
+public class Tile : MonoBehaviour, IPointerDownHandler,IDragHandler
 {
-    /// <summary>
-    /// 处理鼠标按下
-    /// </summary>
-    /// <param name="eventData"></param> <summary>
-    /// 
-    /// </summary>
-    /// <param name="eventData"></param>
+    // ... 其他代码
+
     public void OnPointerDown(PointerEventData eventData)
     {
         // 在这里编写处理点击事件的代码
         Debug.Log("Tile was clicked!");
-        //每次旋转自身 TODO
-        //旋转完写入当前关卡数据 TODO
-        //判断是否满足通关条件 TODO
+
+        // 旋转自身
+        RotateTile();
+
+        // 写入当前关卡数据
+        SaveLevelData();
+
+        // 判断是否满足通关条件
+        CheckWinCondition();
+    }
+
+    private void RotateTile()
+    {
+        // 编写旋转自身的代码
+        // TODO: 实现旋转逻辑
+    }
+
+    private void SaveLevelData()
+    {
+        // 编写写入当前关卡数据的代码
+        // TODO: 实现写入数据逻辑
+    }
+
+    private void CheckWinCondition()
+    {
+        // 编写判断通关条件的代码
+        // TODO: 实现判断通关条件的逻辑
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        // 在拖拽过程中更新位置
+           Debug.Log("Tile was clicked!");
     }
 }
